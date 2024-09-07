e7870a6b88a2d3dbff78ce358bec10981e7fbff5
Shivani Anandan <124684724+ShivaniAnandan@users.noreply.github.com>
2024-09-07 19:10:58 +0530

    Update useLogout.jsx

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 function useLogout() {
     let navigate = useNavigate()
   return()=>{
-        sessionStorage.clear()
+       localStorage.clear()
         navigate('/login')
     }
 }
